0f341175f1bbb79ee3c55f33799afd0d20d6b868
Vladimir Matveev <donald151@gmail.com>
2016-07-03 20:32:56 +0000

    added more examples

--- example/app.js
+++ example/app.js
@@ -9,9 +9,43 @@ window.onload = function () {
     );
     
     ReactDom.render(
-        React.createElement(ReactGauge, {isInnerNumbers: false, aperture: 180}),
+        React.createElement(ReactGauge, {
+            isInnerNumbers: false, 
+            aperture: 180, 
+            arcStrokeWidth: 10,
+            marks: ["а", "б", "в", "г", "д"],
+        }),
         document.querySelector('.container2')  
     );
     
-    
+    ReactDom.render(
+        React.createElement(ReactGauge, {isInnerNumbers: true, aperture: 240, radius: 110}),
+        document.querySelector('.container3')  
+    );
+
+    var clock = ReactDom.render(
+        React.createElement(ReactGauge, {
+            isInnerNumbers: true, 
+            radius: 150, 
+            aperture: 0, 
+            marks: [6, 7, 8, 9, 10, 11, 12, 1 , 2, 3, 4, 5, ""],
+            ranges: [{
+    			start: 0.00001,
+    			end: 0.99999,
+    			color: "#333"
+    		}],
+    		tickLength: 15,
+    		miniTickLength: 7,
+    		tickStrokeWidth: 4,
+    		arcStrokeWidth: 10,
+    		scaleDivisionNumber: 5,
+    		tickLabelOffset: 20,
+    		arrowValue: Math.floor(( new Date().getTime() / 1000 - 30 ) %  60) / 60
+        }),
+        document.querySelector('.container4')  
+    );
+
+    setInterval(function(){
+        clock.setState({arrowValue: Math.floor(( new Date().getTime() / 1000 - 30 ) %  60) / 60});
+    }, 100);
 };
