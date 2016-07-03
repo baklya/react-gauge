ba820f2003c4931b6e09de714bc6d8365bbb3e8d
Vladimir Matveev <donald151@gmail.com>
2016-07-03 18:05:08 +0000

    added browserify

---
+++ example/app.js
@@ -0,0 +1,20 @@
+var React = require('react');
+var ReactDom = require('react-dom');
+var ReactGauge = require('../lib/react-gauge');
+
+var App = React.createClass({
+  displayName: 'App',
+
+  render: function () {
+    return (
+        React.createElement(ReactGauge, {isInnerNumbers: false})
+    )
+  }
+})
+
+window.onload = function () {
+    ReactDom.render(
+        React.createElement(App),
+        document.querySelector('.container')  
+    )
+}
