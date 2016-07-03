344cfc7a856780bea3adf8675212f97c87259a03
Vladimir Matveev <donald151@gmail.com>
2016-07-03 21:10:29 +0000

    adjust svg size

--- example/app.js
+++ example/app.js
@@ -4,7 +4,10 @@ var ReactGauge = require('../lib/react-gauge');
 
 window.onload = function () {
     ReactDom.render(
-        React.createElement(ReactGauge, {isInnerNumbers: false}),
+        React.createElement(ReactGauge, {
+            isInnerNumbers: false,
+            arrowValue: 4/6
+        }),
         document.querySelector('.container1')  
     );
     
@@ -13,13 +16,57 @@ window.onload = function () {
             isInnerNumbers: false, 
             aperture: 180, 
             arcStrokeWidth: 10,
-            marks: ["а", "б", "в", "г", "д"],
+            marks: ["r", "a", "i", "n", "b", "o", "w"],
+            svgContainerHeight: 250,
+            ranges: [{
+    			start: 0,
+    			end: 1/7,
+    			color: "#f00"
+    		},
+    		{
+    			start: 1/7,
+    			end: 2/7,
+    			color: "#ff7f00"
+    		},
+    		{
+    			start: 2/7,
+    			end: 3/7,
+    			color: "#ffff00"
+    		},
+    		{
+    			start: 3/7,
+    			end: 4/7,
+    			color: "#00bc3f"
+    		},
+    		{
+    			start: 4/7,
+    			end: 5/7,
+    			color: "#0068ff"
+    		},
+    		{
+    			start: 5/7,
+    			end: 6/7,
+    			color: "#7a00e5"
+    		},
+    		{
+    			start: 6/7,
+    			end: 1,
+    			color: "#bc31fe"
+    		}]
         }),
         document.querySelector('.container2')  
     );
     
     ReactDom.render(
-        React.createElement(ReactGauge, {isInnerNumbers: true, aperture: 240, radius: 110}),
+        React.createElement(ReactGauge, {
+            isInnerNumbers: true, 
+            aperture: 240, 
+            radius: 110,
+            contentWidth: 250,
+            svgContainerWidth: 350,
+            svgContainerHeight: 180
+            
+        }),
         document.querySelector('.container3')  
     );
 
@@ -40,12 +87,15 @@ window.onload = function () {
     		arcStrokeWidth: 10,
     		scaleDivisionNumber: 5,
     		tickLabelOffset: 20,
-    		arrowValue: Math.floor(( new Date().getTime() / 1000 - 30 ) %  60) / 60
+    		arrowValue: Math.floor(( new Date().getTime() / 1000 - 30 ) %  60) / 60,
+    		contentWidth: 400,
+            svgContainerWidth: 400,
+            svgContainerHeight: 400
         }),
         document.querySelector('.container4')  
     );
 
     setInterval(function(){
-        clock.setState({arrowValue: Math.floor(( new Date().getTime() / 1000 - 30 ) %  60) / 60});
+       // clock.setState({arrowValue: Math.floor(( new Date().getTime() / 1000 - 30 ) %  60) / 60});
     }, 100);
 };
